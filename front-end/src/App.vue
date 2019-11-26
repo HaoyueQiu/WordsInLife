fe381a1825c087db823ccbd176df17c7cfb5ba61
HaoyueQiu <1411434056@qq.com>
2019-11-26 21:49:30 +0800

    login

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Navbar from './components/Navbar'
+  //export 和 export default均用于导出，但是export在引用时要加花括号，而export default可以直接被引用
+  //https://www.jianshu.com/p/edaf43e9384f
+  export default {
+    name: 'App',
+    //运训组件模板递归地调用自身，且便于调试，报错时会报名字，方便定位。
+    // https://cn.vuejs.org/v2/api/#name
+    components:{
+      navbar:Navbar
+    }
+  }
+
 </script>
 
 <style>
+  /*
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +30,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  */
 </style>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,17 +1,70 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Ping from '@/components/Ping'
+import Home from '@/components/Home'
+import Register from '@/components/Register'
+import Login from '@/components/Login'
+import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
 //暴露路由接口
-export default new Router({
+const router =  new Router({
   routes: [
     {
       path: '/',
-      name: 'Ping',
+      name: 'Home',
       // component 代表加载到相应路由上后所使用的组件
+      component: Home,
+      meta:{
+        requireAuth:true
+      }
+    },
+    {
+      path:'/login',
+      name:'Login',
+      component:Login
+    },
+    {
+      path:'/register',
+      name:'Register',
+      component:Register
+    },
+        {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/ping',
+      name: 'Ping',
       component: Ping
     }
   ]
 })
+
+
+//https://router.vuejs.org/zh/guide/advanced/navigation-guards.html#%E5%85%A8%E5%B1%80%E5%89%8D%E7%BD%AE%E5%AE%88%E5%8D%AB
+//导航守卫，以下主要用于判断是否为已登录状态，未登录则请求登录
+router.beforeEach((to,from,next)=>{
+  const token = window.localStorage.getItem('token')
+  if(to.matched.some(record => record.meta.requiresAuth) &&(!token|| token === null)){
+    next({//未登录的用户重定向到登录界面。
+    path: '/login',
+    query: {redirect: to.fullPath}
+    })
+  }else if(token && to.name == 'Login') {
+    //对于已经登录的用户，如果他还要去扽等各路界面那么就进行拦截
+    next({
+    path: from.fullPath
+    })
+  }else {
+    next()
+  }
+})
+
+
+export default router
